7bb19bdf1e7ee9bbb30667bf6a65169fb0a8b32a
MNA-UNAHUR <marcelonahuel.andreassi@estudiantes.unahur.edu.ar>
2026-05-15 20:34:04 -0300

    Creado clase profugo y agregado codigo en cazador

--- src/main/java/ar/edu/unahur/obj2/Cazador.java
+++ src/main/java/ar/edu/unahur/obj2/Cazador.java
@@ -1,7 +1,18 @@
 package ar.edu.unahur.obj2;
 
-public class Cazador {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class Cazador {
+    
+    protected String nombre;
+    protected Integer cantidadDeExperiencia;
+    protected Set<Profugo> profugosCapturados = new HashSet<Profugo>();
+
     public Boolean cazar() {
         return Boolean.TRUE;
     }
+
+
 }
